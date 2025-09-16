9fe935d684aef262cb01173419bea64b4d797fc7
Teja2803 <tejaswini055@gmail.com>
2025-09-16 19:56:24 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,1 @@
-output "frontend_url" {
-  value       = google_cloud_run_service.frontend.status.url
-  description = "Deployed Frontend URL"
-}
 
-output "api_url" {
-  value       = google_cloud_run_service.api.status.url
-  description = "Deployed API URL"
-}
-
-output "sql_instance_ip" {
-  value       = google_sql_database_instance.main.public_ip_address
-  description = "Cloud SQL Public IP"
-}
